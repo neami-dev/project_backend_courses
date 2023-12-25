b0cf030ae5d23e55e742c5ee893ca2acb0a397a4
abdelkabir <abdkabirneami@gmail.com>
2023-12-25 13:23:32 +0100

    modif

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path = require("path");
 
 const port = process.env.PORT;
 const url = process.env.MONGO_URL;
-app.use("/uploads",express.static(path.join(__dirname)))
+app.use(express.static(path.join(__dirname,"uploads")))
 mongoose
     .connect(url)
     .then(() => {
